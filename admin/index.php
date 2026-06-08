a60e801cfee5c2ca98970f3cdc83aeaddffc0922
ADRIANSYAH <109329296+drian-o@users.noreply.github.com>
2026-06-08 13:35:08 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -156,6 +156,9 @@
               } else if ($halaman_aktif == "pengaturan") {
                 include_once "pengaturan.php";
               }
+                else if ($halaman_aktif == "add_domain") {
+                include_once "add_domain.php";
+              }
               include_once "footer.php";
             ?>
           </div>
