bae02d8fb006a6efdc3ec128ba875d386b7bb96b
ADRIANSYAH <109329296+drian-o@users.noreply.github.com>
2026-06-10 15:22:52 +0700

    Update header.php

--- header.php
+++ header.php
@@ -69,6 +69,7 @@ if (isset($seo['redirect_domain']) && $seo['redirect_domain']['isi_2_pengaturan'
     <link rel="amphtml" href="<?php echo $seo['amp_url']['isi_1_pengaturan']; ?>">
 	<meta name="author" content="<?php echo $isi_1_judul_web; ?>" />
 	<meta name="generator" content="<?php echo $alamat_website; ?>" />
+	<link rel="canonical" href="<?php echo $alamat_website; ?>">
 	<meta name="keywords" content="<?php echo $isi_1_judul_web; ?> 88,<?php echo $isi_1_judul_web; ?>" />
 	<meta name="referrer" content="origin-when-cross-origin" />
 	<meta name="color-scheme" content="dark" />
